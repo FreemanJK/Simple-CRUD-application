07b91b7a01ce2cf19291ea876b94c2e8fc600378
FreemanJK <olzhas0544@gmail.com>
2023-05-12 02:59:46 +0600

    Adding postgresql database

--- src/main/java/com/olzhasalishev/spring/config/SpringConfig.java
+++ src/main/java/com/olzhasalishev/spring/config/SpringConfig.java
@@ -2,21 +2,21 @@ package com.olzhasalishev.spring.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.xml.validation.Validator;
 
 @Configuration
 @ComponentScan("com.olzhasalishev.spring")
--- src/main/java/com/olzhasalishev/spring/models/Person.java
+++ src/main/java/com/olzhasalishev/spring/models/Person.java
@@ -1,15 +1,25 @@
 package com.olzhasalishev.spring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
     private int id;
     private String name;
-
-    public Person(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    private int age;
+    private String email;
 
     public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -26,5 +36,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
